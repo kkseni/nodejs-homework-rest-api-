b5719e78f9f360fe893c6b9d626f707a34babbfa
Oksana-Dyka15888 <kseniadika2210@gmail.com>
2023-01-21 09:55:33 +0200

    перевірка токена та зменшення кількості запитів

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,41 +1,14 @@
 const { Contact } = require("../../models");
 
 const getAll = async (req, res, next) => {
-  try {
-    const { _id } = req.user;
-    const { page = 1, limit = 2 } = req.query;
-    const skip = (page - 1) * limit;
-    const contacts = await Contact.find({ owner: _id }, "", {
-      skip,
-      limit: Number(limit),
-    }).populate("owner", "_id email");
-
-    const { favorite } = req.query;
-    const favoriteContacts = await Contact.find(
-      { owner: _id, favorite: favorite },
-      "",
-      { skip, limit: Number(limit) }
-    ).populate("owner", "_id email");
-
-    if (favorite) {
-      // return
-      res.json({
-        status: 200,
-        data: {
-          result: favoriteContacts,
-        },
-      });
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      data: {
-        result: contacts,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 2 } = req.query;
+  const skip = (page - 1) * limit;
+  const contacts = await Contact.find({ owner, ...query }, "", {
+    skip,
+    limit,
+  }).populate("owner", "name email");
+  res.json(contacts);
 };
 
 module.exports = getAll;
--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -12,7 +12,9 @@ const authenticate = async (req, res, next) => {
       throw RequestError(401, "Not authorized");
     }
     try {
-      const { id } = jwt.verify(token, SECRET_KEY);
+      if (user.token = token) { 
+     const { id } = jwt.verify(token, SECRET_KEY);
+    }
       const user = await User.findById(id);
       if (!user || !user.token) {
         throw Error("Not authorized");
